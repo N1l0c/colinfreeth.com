f89e1bfba4e7ee02ad9b432f9c85737f65d08716
Colin Freeth <colin.freeth@protonmail.com>
2025-03-24 23:14:58 +0000

    Added trailing slash to site: 'https://colinfreeth.com/blog/', in astro.config.mjs

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://colinfreeth.com/blog', // ✅ You can change this to 'https://colinfreeth.com' for canonical URLs
+	site: 'https://colinfreeth.com/blog/', // ✅ You can change this to 'https://colinfreeth.com' for canonical URLs
 	integrations: [mdx(), sitemap()],
 	trailingSlash: 'always'      // ✅ Add this for reliable routing on Vercel
 });
