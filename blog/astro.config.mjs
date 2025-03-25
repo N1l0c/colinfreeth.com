639a86ae217c5d96b0cf5c1135d11a92e629f26b
Colin Freeth <colin.freeth@protonmail.com>
2025-03-25 18:22:01 +0000

    Updated site URL to correct formatting in astro.config.mjs

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'blog.colinfreeth.com/',
+	site: 'https://blog.colinfreeth.com/',
 	integrations: [mdx(), sitemap()],
 });
