37171ca7d06f0781f55a6315468555a29d958edc
Colin Freeth <colin.freeth@protonmail.com>
2025-03-30 13:10:11 +0100

    Refactor entire website to Astro

---
+++ src/components/NAlityWrapper.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react';
+import App from 'n-ality-av/App';
+
+const NAlityWrapper: FC = () => (
+  <div className="n-ality-wrapper">
+    <App />
+  </div>
+);
+
+export default NAlityWrapper;
